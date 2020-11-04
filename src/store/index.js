3b77cf6ca49311b1d32a59ae02887505935eb8d7
hashimtbox <70660872+hashimtbox@users.noreply.github.com>
2020-11-04 16:21:58 +0500

    add perisit

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ import storage from 'redux-persist/lib/storage' // defaults to localStorage for 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['products']
+  // blacklist: ['products']
 }
 
 const rootReducer = combineReducers({
