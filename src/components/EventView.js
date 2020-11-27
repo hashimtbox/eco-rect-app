241602d908c06137de8f9be0a1b2d1ee60acefac
HamzaAzeem97 <66351978+HamzaAzeem97@users.noreply.github.com>
2020-11-27 07:53:31 +0500

    fixed issue of card height

--- src/components/EventView.js
+++ src/components/EventView.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Skeleton from '@material-ui/lab/Skeleton';
+import Skeleton from "@material-ui/lab/Skeleton";
 import {
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
-  Typography
+  Typography,
 } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import authSlice from "../store/auth";
@@ -14,10 +14,13 @@ import jeans from "../assets/jeans.jpg";
 import { Link } from "react-router-dom";
 
 const EventView = ({ product }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
-    <Card>
-      <Link style={{ textDecoration: "none", color: "black" }} to={`/products/detail/${product?.id}`}>
+    <Card style={{ height: "100%" }}>
+      <Link
+        style={{ textDecoration: "none", color: "black" }}
+        to={`/products/detail/${product?.id}`}
+      >
         <CardMedia
           style={{ height: 150, minWidth: 200 }}
           image={product?.main_image || jeans}
@@ -25,18 +28,28 @@ const EventView = ({ product }) => {
         />
       </Link>
 
-      <CardContent>
-        <div style={{ display: "flex", height: 60, flexGrow: 1 }}>
-          <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      <CardContent style={{ paddingBottom: 0 }}>
+        <div style={{ display: "flex", flexGrow: 1 }}>
+          <div
+            style={{ display: "flex", flexDirection: "column", width: "100%" }}
+          >
             <Typography gutterBottom variant="h5" component="h2">
-              <Link style={{ textDecoration: "none", color: "black" }} to={`/products/detail/${product?.id}`}>{product?.title}</Link>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to={`/products/detail/${product?.id}`}
+              >
+                {product?.title}
+              </Link>
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               {product?.description.slice(0, 100).toLowerCase()}
             </Typography>
 
             <div style={{ marginTop: 8 }}>
-              <Typography variant='h5' style={{ fontSize: 13.5, fontWeight: 500 }}>
+              <Typography
+                variant="h5"
+                style={{ fontSize: 13.5, fontWeight: 500 }}
+              >
                 {`Price : $ ${product?.price}.00`}
               </Typography>
             </div>
@@ -49,9 +62,8 @@ const EventView = ({ product }) => {
         {/*<Button size="small" color="secondary" onClick={() => dispatch(authSlice.actions.addToCart(product))}>*/}
         {/*  Add to Cart*/}
         {/*</Button>*/}
-
       </CardActions>
-    </Card >
+    </Card>
   );
 };
 
